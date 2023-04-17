81e655055b0368bf8475ff00d4540e9f31616932
asif0927 <6zd6jws@code.edu.az>
2023-04-18 03:54:40 +0400

    3task added equal siqn

--- task3/script.js
+++ task3/script.js
@@ -10,16 +10,16 @@ console.log(monthNum(12));
 /*Anonim */
 const anonimMonthNum=function(num){
     let month = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-    if(num<0||num>12){
+    if(num<=0||num>12){
         return "invalid input"
     }
     return month[num-1];
 }
-console.log(anonimMonthNum(-7));
+console.log(anonimMonthNum(0));
 /*Arrow */
 arrowMonthNum=(num)=>{
     let month = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-    if(num<0||num>12){
+    if(num<=0||num>12){
         return "invalid input"
     }
     return month[num-1];
